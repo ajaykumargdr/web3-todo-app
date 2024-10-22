84fa476a97af1e1dbacef7665b42a0abcc57a5bf
ajaykumargdr <ajaykumarncas@gmail.com>
2024-10-23 01:25:39 +0530

    chore: add contract address and abi constants

---
+++ context/constants.js
@@ -0,0 +1,5 @@
+import ToDoList from "../artifacts/contracts/ToDoList.sol/ToDoList.json";
+
+// export const TODO_LIST_CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+export const TODO_LIST_CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const TODO_LIST_CONTRACT_ABI = ToDoList.abi;
