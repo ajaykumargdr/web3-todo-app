a24727a60e26e5f22a565cb328c64249be3015c7
ajaykumargdr <ajaykumarncas@gmail.com>
2024-11-08 00:42:34 +0530

    chore: update contract id with localhost contract address

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,5 @@
 import ToDoList from "../artifacts/contracts/ToDoList.sol/ToDoList.json";
 
 // export const TODO_LIST_CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
-export const TODO_LIST_CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const TODO_LIST_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const TODO_LIST_CONTRACT_ABI = ToDoList.abi;
