8da257fd46343ff85e97ae0094e6ec8c06079757
ajaykumargdr <ajaykumarncas@gmail.com>
2024-11-08 00:02:47 +0530

    fix: resolve bad_data error

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,12 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
-  
+  defaultNetwork: "localhost",
   networks: {
-    hardhat: {
-      chainId: 1337,
+    localhost: {
+      url: "http://127.0.0.1:8545",  // This connects to the Hardhat node
     },
-  }
+  },
 };
 
 task("accounts", "Prints the list of accounts", async () => {
