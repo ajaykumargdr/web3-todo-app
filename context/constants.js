e0a893d0bc73e33861e366d1f2615397a93debfc
ajaykumargdr <ajaykumarncas@gmail.com>
2024-12-05 22:34:57 +0530

    chore: remove hardcoded value of contract address & add configuration file for contract address

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,6 @@
 import ToDoList from "../artifacts/contracts/ToDoList.sol/ToDoListApp.json";
+import contract_address from "@/contract_address.json"
 
 // export const TODO_LIST_CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
-export const TODO_LIST_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const TODO_LIST_CONTRACT_ADDRESS = contract_address.address;
 export const TODO_LIST_CONTRACT_ABI = ToDoList.abi;
