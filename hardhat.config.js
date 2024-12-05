aa54f6469b06fbdcd9112892cf9eb980e7dac5d6
ajaykumargdr <ajaykumarncas@gmail.com>
2024-12-05 22:35:40 +0530

    chore: update eth sepolia network configuration in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,10 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545",  // This connects to the Hardhat node
     },
+    sepolia:{
+      url: "https://eth-sepolia.g.alchemy.com/v2/N2EUMkuqU2JqyiNsoMBFumwh4BYw41dm",
+      accounts: ["e30d29df952d96c7314cc11c8fe38aa8ca93e22e56b63d3aed4a7cb122a28795"]
+    }
   },
 };
 
